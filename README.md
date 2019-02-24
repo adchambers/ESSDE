70ef74932b9c0ffac6ba6feb393ba803dc46be6f
Adam <adchambers@live.com>
2019-02-24 12:54:02 -0600

    Fine tuned verbiage

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 /* 
-    Adam Chambers 
-    This repo contains files for a course that I'm taking on Coursera.
+    # Adam Chambers
+    ## This repo contains files for a course that I'm taking on Coursera.
 */
 
--- stats.c
+++ stats.c
@@ -1,13 +1,3 @@
-/******************************************************************************
- * Copyright (C) 2019 by Adam Chambers
- *
- * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
- * permitted to modify this and use it to learn about the field of embedded
- * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
- *
- *****************************************************************************/
 /**
  * @file <stats.c> 
  * @brief <This simple applicatoin performs statistical analytics on a dataset.>
